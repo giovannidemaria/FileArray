35b41d937d2b7c872bd53a0e7a3834076fe54b02
giovannidemaria <giovidemaria@gmail.com>
2023-04-19 08:58:29 +0200

    Initial commit

---
+++ src/FileArray.java
@@ -0,0 +1,15 @@
+public class FileArray {
+  // loads an existing file
+  public FileArray(String filePathName) { /* ... */ }
+
+  // creates new file with n random elements
+  public FileArray(String filePathName, int n) { /* ... */ }
+
+  // pretty print with at most 5 aligned elements per row
+  public void print() { /* ... */ }
+
+  //increment all elements
+  public void incrementAll() { /* ... */ }
+
+
+}
